6a3f79ac1356d6ff9273d6e9492e1804f3fac77b
Worker-B <workerb@new-host.home>
2017-01-19 14:14:16 -0800

    New branch to test two new functions.

--- GitPractice3/GitPractice3/ViewController.swift
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func testFunction1() {
+        print("test1")
     }
-
-
+    func testFunction2() {
+        print("test2")
+    }
 }
 

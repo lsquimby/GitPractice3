889008e4857b180c60800c7f6de4746afc354256
Worker-B <workerb@lawrences-imac.home>
2017-01-19 14:01:56 -0800

    Added new project.

---
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice3
+//
+//  Created by Worker-B on 1/19/17.
+//  Copyright © 2017 Very Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
